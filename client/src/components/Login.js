9cc2c42229fc8b931511ab120056ecbc73f692bc
jebediahelliott <jebediahelliott@gmail.com>
2018-12-10 13:40:24 -0500

    Set up login form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap'
 
-const Login = props => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <div style={{margin: '10%'}}>
+        <form style={{maxWidth: '300px'}}>
+          <FormGroup
+           controlId="loginForm"
+
+          >
+           <ControlLabel>Email</ControlLabel>
+           <FormControl
+             type="text"
+             name="email"
+             value={this.state.email}
+             onChange={this.handleChange}
+           />
+           <ControlLabel>Password</ControlLabel>
+           <FormControl
+             type="password"
+             name="password"
+             value={this.state.password}
+             onChange={this.handleChange}
+           />
+           <HelpBlock>Password must be at least 6 characters.</HelpBlock>
+         </FormGroup>
+         <Button type="submit">Log In</Button>
+        </form>
+      </div>
+    )
+  }
 }
+
+// validationState={this.getValidationState()}
 
 export default Login;
