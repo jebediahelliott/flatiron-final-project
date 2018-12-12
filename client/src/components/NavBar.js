befb7992a2ab0d433950be183127d283dcb1a0a6
jebediahelliott <jebediahelliott@gmail.com>
2018-12-11 21:06:52 -0500

    Add styling to nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,62 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import {Navbar, Nav, NavItem } from 'react-bootstrap';
+import './NavBar.css'
 
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-  alignSelf: 'flex-end'
-}
+
 
 const NavBar = () => {
   return (
-    <div>
-      <Navbar>
-        <Nav>
-          <NavItem eventKey={1}>
-            <NavLink to='/'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >Home</NavLink>
-          </NavItem>
-          <NavItem eventKey={2}>
-            <NavLink to='/about'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >About</NavLink>
-          </NavItem>
-          <NavItem eventKey={3}>
-            <NavLink to='/training-programs'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >Training Programs</NavLink>
-          </NavItem>
-          <NavItem eventKey={4}>
-            <NavLink to='/faq'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >FAQ</NavLink>
-          </NavItem>
-          <NavItem eventKey={5}>
-            <NavLink to='/contact'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >Contact Us</NavLink>
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem>
-            <NavLink to='/login'
-            exact
-            // activeStyle={{background: 'lightgreen'}}
-            >Log In</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+    <div id='navBar'>
+      <NavLink to='/'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >Home</NavLink>
+      <NavLink to='/about'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >About</NavLink>
+      <NavLink to='/training-programs'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >Training Programs</NavLink>
+      <NavLink to='/faq'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >FAQ</NavLink>
+      <NavLink to='/contact'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >Contact Us</NavLink>
+      <NavLink to='/login'
+      exact
+      className='navLink'
+      activeStyle={{background: '#33691e', color: 'white'}}
+      >Log In</NavLink>
     </div>
   )
 }
